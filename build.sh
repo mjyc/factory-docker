397d25a2e507a6eb6a15c633d3b7b626e0331344
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-05-19 22:09:05 -0700

    Take arg to 'factory' dir

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ] || [ ! -d "$1" ]; then
+  echo "usage: build.sh [factory_dir]";
+fi
+
 CUR_DIR="$(cd "$(dirname $0)" && pwd)"
 BUILDS_DIR=${CUR_DIR}/build/
 
-cd ${CUR_DIR}/factory && meteor build ${BUILDS_DIR} --directory
+cd $1 && meteor build ${BUILDS_DIR} --directory
 
 cd ${BUILDS_DIR} && docker build -t factory_app .
 
