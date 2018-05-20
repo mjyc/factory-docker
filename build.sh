504b6f85a76e07a23e02f391a93517f6e9ac2e4a
Michael Jae-Yoon Chung <mjyc@cs.washington.edu>
2018-05-19 22:12:49 -0700

    Revert "Take arg to 'factory' dir"
    This reverts commit 397d25a2e507a6eb6a15c633d3b7b626e0331344.

--- build.sh
+++ build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ] || [ ! -d "$1" ]; then
-  echo "usage: build.sh [factory_dir]";
-fi
-
 CUR_DIR="$(cd "$(dirname $0)" && pwd)"
 BUILDS_DIR=${CUR_DIR}/build/
 
-cd $1 && meteor build ${BUILDS_DIR} --directory
+cd ${CUR_DIR}/factory && meteor build ${BUILDS_DIR} --directory
 
 cd ${BUILDS_DIR} && docker build -t factory_app .
 
